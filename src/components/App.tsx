8ed253d5383718ab327e07a9a0dfdcebf1afaec5
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-09-16 23:26:50 +0300

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-[35px] text-[#F6B83D]">PetLove</h1>
+    </div>
+  );
+};
