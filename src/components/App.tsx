da78a7898388f73a921151342b37e74c4779131b
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-09-19 02:33:26 +0300

    add routes lazy loading for routes and implement application routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,32 @@
+import { lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import { Layout } from "../components";
+
+const HomePage = lazy(() => import("../pages/HomePage"));
+const NewsPage = lazy(() => import("../pages/NewsPage"));
+const NoticesPage = lazy(() => import("../pages/NoticesPage"));
+const FriendsPage = lazy(() => import("../pages/FriendsPage"));
+const RegistrationPage = lazy(() => import("../pages/RegistrationPage"));
+const LoginPage = lazy(() => import("../pages/LoginPage"));
+const ProfilePage = lazy(() => import("../pages/ProfilePage"));
+const AddPetPage = lazy(() => import("../pages/AddPetPage"));
+const NotFoundPage = lazy(() => import("../pages/NotFoundPage"));
+
 export const App = () => {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-[35px] text-[#F6B83D]">PetLove</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/news" element={<NewsPage />} />
+        <Route path="/notices" element={<NoticesPage />} />
+        <Route path="/friends" element={<FriendsPage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/add-pet" element={<AddPetPage />}></Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   );
 };
