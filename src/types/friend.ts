5c87f6d7a51eaca157ab930fb9e96e16e8efa62b
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-09-26 21:00:11 +0300

    add friend type

---
+++ src/types/friend.ts
@@ -0,0 +1,18 @@
+export interface IWorkDay {
+  // _id: string;
+  isOpen: boolean;
+  from?: string;
+  to?: string;
+}
+
+export interface IFriend {
+  // _id: string;
+  title: string;
+  url: string;
+  addressUrl: string;
+  imageUrl: string;
+  address: string;
+  workDays: IWorkDay[];
+  phone: string;
+  email: string;
+}
