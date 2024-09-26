df1b576199b16b256db8a4545e94a29d96315a22
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-09-26 21:06:27 +0300

    update title

--- src/pages/FriendsPage.tsx
+++ src/pages/FriendsPage.tsx
@@ -3,7 +3,7 @@ import { FriendsList, Title } from "../components";
 const FriendsPage = () => {
   return (
     <div className="container pb-[80px] pt-[34px] md:pt-[46px] lg:pt-[64px]">
-      <Title title="Find your favorite pet" className="mb-10 lg:mb-10" />
+      <Title title="Our friends" className="mb-10 lg:mb-[60px]" />
       <FriendsList />
     </div>
   );
--- src/pages/NoticesPage.tsx
+++ src/pages/NoticesPage.tsx
@@ -3,7 +3,7 @@ import { Title } from "../components";
 const NoticesPage = () => {
   return (
     <div className="container pb-[80px] pt-[34px] md:pb-[80px] md:pt-[46px] lg:pt-[64px]">
-      <Title title="Our friends" className="mb-10 lg:mb-[60px]" />
+      <Title title="Find your favorite pet" className="mb-10 lg:mb-10" />
     </div>
   );
 };
