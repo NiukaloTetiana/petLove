681c2fc9a261df68913a4350d3100896194a8c8f
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-09-30 23:53:51 +0300

    change type

--- src/types/new.ts
+++ src/types/new.ts
@@ -1,13 +1,14 @@
-export type New = {
-  results: Array<{
-    _id: string;
-    imgUrl: string;
-    title: string;
-    text: string;
-    date: string;
-    url: string;
-  }>;
+export type INewItem = {
+  _id: string;
+  imgUrl: string;
+  title: string;
+  text: string;
+  date: string;
+  url: string;
+};
 
+export type INew = {
+  results: INewItem[];
   totalPages: number;
   page: number;
   perPage: number;
