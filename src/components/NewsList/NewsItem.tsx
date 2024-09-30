826a2f641ae71b22713392424285ccdf9b7c0a04
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-09-30 19:52:24 +0300

    add props

---
+++ src/components/NewsList/NewsItem.tsx
@@ -0,0 +1,3 @@
+export const NewsItem = () => {
+  return <div>NewsItem</div>;
+};
--- src/components/NoticesList/NoticesList.tsx
+++ src/components/NoticesList/NoticesList.tsx
@@ -2,15 +2,20 @@
 import { NoticesItem } from "./NoticesItem";
 import notices from "../../assets/notices.json";
 
-// interface INoticesListProps {
-//   notices: INotice[];
-// }
+interface INoticesListProps {
+  // notices: INotice[];
+  toggleNoticeModal: () => void;
+}
 
-export const NoticesList = () => {
+export const NoticesList = ({ toggleNoticeModal }: INoticesListProps) => {
   return (
     <ul className="flex flex-wrap gap-5 lg:gap-x-[32px] lg:gap-y-10">
       {notices.results.map((notice, index) => (
-        <NoticesItem key={index} notice={notice} />
+        <NoticesItem
+          key={index}
+          notice={notice}
+          toggleNoticeModal={toggleNoticeModal}
+        />
       ))}
     </ul>
   );
