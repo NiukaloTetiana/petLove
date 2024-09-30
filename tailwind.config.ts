84300351b92b0b035462f979b02f06a0242bbcca
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-10-01 00:05:31 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,7 +33,7 @@ export default {
           },
           "@screen lg": {
             paddingLeft: "64px",
-            paddingRight: "64px",
+            paddingRight: "63px",
             maxWidth: "1280px",
           },
         },
