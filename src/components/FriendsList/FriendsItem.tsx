78b9d7ee4dcedd064c043b461e45d1073919f28f
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-10-11 01:24:35 +0300

    FriendsItem update styles

--- src/components/FriendsList/FriendsItem.tsx
+++ src/components/FriendsList/FriendsItem.tsx
@@ -9,6 +9,29 @@ export const FriendsItem = ({ friend }: IFriendsItemProps) => {
   const { title, url, addressUrl, imageUrl, address, workDays, phone, email } =
     friend;
 
+  const renderField = (label: string, value?: string, href?: string) => (
+    <>
+      {value ? (
+        href ? (
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={href}
+            className="a-hover"
+          >
+            <span className="text-[#2626267f]">{label}: </span> {value}
+          </a>
+        ) : (
+          value
+        )
+      ) : (
+        <>
+          <span className="text-[#2626267f]">{label}: </span>Not available
+        </>
+      )}
+    </>
+  );
+
   return (
     <li className="relative flex w-full gap-[14px] rounded-[15px] bg-white px-5 py-10 sm-max:gap-[8px] md:w-[342px] md:gap-[16px] lg:w-[371px] lg:gap-5">
       <div className="flex h-[80px] w-[80px] shrink-0 items-center justify-center overflow-hidden rounded-full sm-max:h-[60px] sm-max:w-[60px] md:h-[75px] md:w-[75px] lg:h-[90px] lg:w-[90px]">
@@ -33,38 +56,14 @@ export const FriendsItem = ({ friend }: IFriendsItemProps) => {
 
         <address>
           <ul className="flex w-full flex-col gap-2">
-            <li className="item">
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href={`mailto:${email}`}
-                className="a-hover"
-              >
-                <span className="text-[#2626267f]">Email: </span>
-                {email}
-              </a>
+            <li className="item-friends">
+              {renderField("Email", email, `mailto:${email}`)}
             </li>
-            <li className="item">
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href={addressUrl}
-                className="a-hover"
-              >
-                <span className="text-[#2626267f]">Address: </span>
-                {address}
-              </a>
+            <li className="item-friends">
+              {renderField("Address", address, addressUrl)}
             </li>
-            <li className="item">
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href={`tel:${phone}`}
-                className="a-hover"
-              >
-                <span className="text-[#2626267f]">Phone: </span>
-                {phone}
-              </a>
+            <li className="item-friends">
+              {renderField("Phone", phone, `tel:${phone}`)}
             </li>
           </ul>
         </address>
