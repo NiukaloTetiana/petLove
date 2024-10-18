8ccfd3e040181da17271e9adc98a3005d931287f
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-10-19 00:53:16 +0300

    update refreshUser thunk

--- src/redux/auth/authOperations.ts
+++ src/redux/auth/authOperations.ts
@@ -8,6 +8,7 @@ import {
   ILoginRequest,
   IRefreshResponse,
 } from "../../types";
+import { RootState } from "../store";
 
 export const registerUser = createAsyncThunk<
   IAuthResponse,
@@ -62,16 +63,26 @@ export const logoutUser = createAsyncThunk<
 export const refreshUser = createAsyncThunk<
   IRefreshResponse,
   undefined,
-  { rejectValue: string }
->("auth/refresh", async (_, { rejectWithValue }) => {
+  { state: RootState; rejectValue: string }
+>("auth/refresh", async (_, thunkAPI) => {
+  const {
+    auth: { token },
+  } = thunkAPI.getState();
+
+  if (!token) {
+    return thunkAPI.rejectWithValue("There is no refreshToken");
+  }
+
   try {
+    setToken(token);
     const { data } = await instance.get("/users/current");
+
     setToken(data.token);
 
     return data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
-      return rejectWithValue(error.response?.data.message);
+      return thunkAPI.rejectWithValue(error.response?.data.message);
     }
   }
 });
