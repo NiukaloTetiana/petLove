b36b8e1adabe3b2d97d95bddeded28402ef34417
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-10-21 23:45:40 +0300

    change selector

--- src/components/PetsList/PetsList.tsx
+++ src/components/PetsList/PetsList.tsx
@@ -1,13 +1,13 @@
 import { useAppSelector } from "../../hooks";
-import { selectPets } from "../../redux";
+import { selectUser } from "../../redux";
 import { PetsItem } from "./PetsItem";
 
 export const PetsList = () => {
-  const pets = useAppSelector(selectPets);
+  const user = useAppSelector(selectUser);
 
   return (
     <ul className="mb-5 flex flex-wrap justify-center gap-[14px] lg:mb-10">
-      {pets.map((pet) => (
+      {user.pets.map((pet) => (
         <PetsItem key={pet._id} pet={pet} />
       ))}
     </ul>
