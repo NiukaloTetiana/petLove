5eda713bbba5afae39c818795fb6f7a97437eb9a
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-10-23 22:46:20 +0300

    add logic to display avatar for logged-in user

--- src/components/Header/UserBar.tsx
+++ src/components/Header/UserBar.tsx
@@ -6,6 +6,7 @@ import { selectUser } from "../../redux";
 
 export const UserBar = () => {
   const user = useAppSelector(selectUser);
+
   const location = useLocation();
   const isHomePage = location.pathname === "/";
 
@@ -16,13 +17,21 @@ export const UserBar = () => {
       <div className="flex items-center justify-center gap-[8px]">
         <Link
           to="/profile"
-          className="link-reg flex size-10 items-center justify-center rounded-[50%] bg-[#fff4df] transition duration-500 md:size-[50px]"
+          className="link-reg flex size-10 shrink-0 items-center justify-center overflow-hidden rounded-full bg-[#fff4df] transition duration-500 md:size-[50px]"
         >
-          <Icon
-            id="user"
-            size={20}
-            className="fill-[#f6b83d] stroke-[#f6b83d] md:size-[24px]"
-          />
+          {!user.avatar ? (
+            <Icon
+              id="user"
+              size={20}
+              className="fill-[#f6b83d] stroke-[#f6b83d] md:size-[24px]"
+            />
+          ) : (
+            <img
+              src={user.avatar}
+              alt={user.name || ""}
+              className="h-full w-full object-cover"
+            />
+          )}
         </Link>
         <p
           className={`hidden md:block md:text-[20px] md:font-bold md:leading-[1] md:tracking-[-0.03em] ${isHomePage ? "text-white" : "text-[#262626]"}`}
