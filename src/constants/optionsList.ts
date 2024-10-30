634b2fe94d32cca782a3ff6dc434a4362f3a1d22
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-10-31 01:29:34 +0200

    add optionsList constant

---
+++ src/constants/optionsList.ts
@@ -0,0 +1,6 @@
+export const optionsList = [
+  { label: "Popular", value: "byPopularity=false" },
+  { label: "Unpopular", value: "byPopularity=true" },
+  { label: "Cheap", value: "byPrice=true" },
+  { label: "Expensive", value: "byPrice=false" },
+];
