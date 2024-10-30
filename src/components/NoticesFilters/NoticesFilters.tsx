2b917285b3ccf6b0fbb3267e59bfcf577c7cac31
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-10-30 02:19:34 +0200

    update NoticesFilters

--- src/components/NoticesFilters/NoticesFilters.tsx
+++ src/components/NoticesFilters/NoticesFilters.tsx
@@ -1,18 +1,72 @@
-// import { useAppSelector } from "../../hooks";
-// import { selectCategories, selectSex, selectSpecies } from "../../redux";
-import { SearchField } from "../SearchField/SearchField";
+import { useState } from "react";
+
+import { useAppSelector } from "../../hooks";
+import { selectCategories, selectSex, selectSpecies } from "../../redux";
+import { DropdownSelect, SearchField } from "../../components";
 
 export const NoticesFilters = () => {
-  // const caterogies = useAppSelector(selectCategories);
-  // const sex = useAppSelector(selectSex);
-  // const species = useAppSelector(selectSpecies);
+  const caterogies = useAppSelector(selectCategories);
+  const sex = useAppSelector(selectSex);
+  const species = useAppSelector(selectSpecies);
+
+  const [category, setCategory] = useState("Show all");
+  const [gender, setGender] = useState("Show all");
+  const [type, setType] = useState("Show all");
+
   const handleSearch = () => {};
 
   const handleChange = () => {};
 
+  console.log(category);
+  console.log(gender);
+  console.log(type);
+
   return (
     <div className="rounded-[30px] bg-[#fff4df] p-5 md:px-[32px] md:py-10 lg:px-10">
-      <SearchField onChange={handleChange} onSearch={handleSearch} />
+      <div className="mb-10 flex flex-wrap gap-[12px] md:gap-[16px]">
+        <SearchField onChange={handleChange} onSearch={handleSearch} />
+        <DropdownSelect
+          options={caterogies}
+          setOption={setCategory}
+          defaultOption={"Show all"}
+          placeholder="Category"
+          className="w-[141px] capitalize sm-max:w-full md:w-[170px] lg:w-[190px]"
+        />
+
+        <DropdownSelect
+          options={sex}
+          setOption={setGender}
+          defaultOption={"Show all"}
+          placeholder="By gender"
+          className="w-[141px] capitalize sm-max:w-full md:w-[170px] lg:w-[190px]"
+        />
+
+        <DropdownSelect
+          options={species}
+          setOption={setType}
+          defaultOption={"Show all"}
+          placeholder="By type"
+          className="w-full capitalize md:w-[190px]"
+        />
+      </div>
+
+      <div className="flex flex-wrap gap-[10px] md:gap-[6px]">
+        <button type="button" className="notices-button">
+          Popular
+        </button>
+
+        <button type="button" className="notices-button">
+          Unpopular
+        </button>
+
+        <button type="button" className="notices-button">
+          Expensive
+        </button>
+
+        <button type="button" className="notices-button">
+          Cheap
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/SearchField/SearchField.tsx
+++ src/components/SearchField/SearchField.tsx
@@ -29,12 +29,14 @@ export const SearchField = ({
   // };
 
   return (
-    <div className="relative">
+    <div
+      className={`relative w-full ${isNewsPage ? "md:w-[230px]" : "md:w-[265px]"}`}
+    >
       <input
-        className={`h-[42px] w-full rounded-[30px] p-[12px] pr-[50px] text-[14px] font-medium leading-[1.29] tracking-[-0.03em] text-[#262626] transition duration-500 hover:shadow-md focus:shadow-md md:h-[48px] md:p-[14px] md:text-[16px] md:leading-[1.25] ${
+        className={`h-[42px] w-full rounded-[30px] p-[12px] pr-[50px] text-[14px] font-medium leading-[1.29] tracking-[-0.03em] text-[#262626] transition duration-500 hover:shadow-md focus:shadow-md md:h-[48px] md:p-[14px] md:pr-[54px] md:text-[16px] md:leading-[1.25] ${
           isNewsPage
-            ? "border border-[#26262626] bg-transparent placeholder:text-[#2626267f] hover:border-[#f6b83d] focus:border-[#f6b83d] md:w-[230px]"
-            : "border-none bg-white md:w-[265px]"
+            ? "border border-[#26262626] bg-transparent placeholder:text-[#2626267f] hover:border-[#f6b83d] focus:border-[#f6b83d]"
+            : "border-none bg-white placeholder:text-[#262626]"
         }`}
         name="search"
         placeholder="Search"
