213ecfb87c891bcf38a224a7be189e9f15d21a77
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-11-06 22:35:55 +0200

    add nested routes for favorites and viewed pages

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 
 import { UserCard } from "../components";
 import { useAppDispatch } from "../hooks";
 import { getUserCurrent } from "../redux";
+import { NavLink, Outlet } from "react-router-dom";
 
 const ProfilePage = () => {
   const dispatch = useAppDispatch();
@@ -12,8 +13,22 @@ const ProfilePage = () => {
   }, [dispatch]);
 
   return (
-    <div className="container pb-[80px] lg:pb-[64px]">
+    <div className="container flex flex-col gap-[32px] pb-[80px] lg:flex-row lg:gap-10 lg:px-[32px] lg:pb-[64px]">
       <UserCard />
+
+      <div className="lg:w-[664px]">
+        <div className="mb-5 flex items-center gap-[10px] md:gap-[8px] lg:mb-[32px]">
+          <NavLink to="favorites" className="link-profile">
+            My favorite pets
+          </NavLink>
+          <NavLink to="viewed" className="link-profile">
+            Viewed
+          </NavLink>
+        </div>
+        <Suspense>
+          <Outlet />
+        </Suspense>
+      </div>
     </div>
   );
 };
