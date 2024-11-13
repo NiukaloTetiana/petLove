b232f1cdddda52436abd1444c0fa7dbdcf82a7ee
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-11-13 02:04:16 +0200

    update styles

--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -5,25 +5,21 @@ import { NoticesItem } from "../NoticesList/NoticesItem";
 const Favorites = () => {
   const noticesFavorites = useAppSelector(selectNoticesFavorites);
 
-  return (
-    <div className="flex items-center justify-center">
-      {noticesFavorites.length === 0 ? (
-        <h4 className="text-center text-[14px] font-medium leading-[1.29] tracking-[-0.02em] text-[#262626] md:w-[458px] md:text-[16px] md:leading-[1.25]">
-          Oops,{" "}
-          <span className="font-bold text-[#f6b83d]">
-            looks like there aren't any furries
-          </span>{" "}
-          on our adorable page yet. Do not worry! View your pets on the "find
-          your favorite pet" page and add them to your favorites.
-        </h4>
-      ) : (
-        <ul className="scrollbar flex flex-col gap-5 md:flex-row md:flex-wrap lg:max-h-[796px]">
-          {noticesFavorites.map((notice) => (
-            <NoticesItem key={notice._id} notice={notice} />
-          ))}
-        </ul>
-      )}
-    </div>
+  return noticesFavorites.length === 0 ? (
+    <h4 className="mx-auto mt-[60px] text-center text-[14px] font-medium leading-[1.29] tracking-[-0.02em] text-[#262626] md:mt-[160px] md:w-[458px] md:text-[16px] md:leading-[1.25]">
+      Oops,{" "}
+      <span className="font-bold text-[#f6b83d]">
+        looks like there aren't any furries
+      </span>{" "}
+      on our adorable page yet. Do not worry! View your pets on the "find your
+      favorite pet" page and add them to your favorites.
+    </h4>
+  ) : (
+    <ul className="scrollbar flex flex-col gap-5 md:flex-row md:flex-wrap lg:max-h-[796px]">
+      {noticesFavorites.map((notice) => (
+        <NoticesItem key={notice._id} notice={notice} />
+      ))}
+    </ul>
   );
 };
 
--- src/components/Viewed/Viewed.tsx
+++ src/components/Viewed/Viewed.tsx
@@ -5,25 +5,21 @@ import { NoticesItem } from "../NoticesList/NoticesItem";
 const Viewed = () => {
   const noticesViewed = useAppSelector(selectNoticesViewed);
 
-  return (
-    <div className="flex items-center justify-center">
-      {noticesViewed.length === 0 ? (
-        <h4 className="mx-auto text-center text-[14px] font-medium leading-[1.29] tracking-[-0.02em] text-[#262626] md:w-[458px] md:text-[16px] md:leading-[1.25]">
-          Oops,{" "}
-          <span className="font-bold text-[#f6b83d]">
-            it looks like you haven't viewed any furry friends
-          </span>{" "}
-          on our page yet. Start exploring and check out some adorable pets.
-          Your recently viewed items will appear here!
-        </h4>
-      ) : (
-        <ul className="scrollbar flex flex-col gap-5 md:flex-row md:flex-wrap lg:max-h-[796px]">
-          {noticesViewed.map((notice) => (
-            <NoticesItem key={notice._id} notice={notice} />
-          ))}
-        </ul>
-      )}
-    </div>
+  return noticesViewed.length === 0 ? (
+    <h4 className="mx-auto mt-[60px] text-center text-[14px] font-medium leading-[1.29] tracking-[-0.02em] text-[#262626] md:mt-[160px] md:w-[458px] md:text-[16px] md:leading-[1.25]">
+      Oops,{" "}
+      <span className="font-bold text-[#f6b83d]">
+        it looks like you haven't viewed any furry friends
+      </span>{" "}
+      on our page yet. Start exploring and check out some adorable pets. Your
+      recently viewed items will appear here!
+    </h4>
+  ) : (
+    <ul className="scrollbar flex flex-col gap-5 md:flex-row md:flex-wrap lg:max-h-[796px]">
+      {noticesViewed.map((notice) => (
+        <NoticesItem key={notice._id} notice={notice} />
+      ))}
+    </ul>
   );
 };
 
